ba0fd4c44a47f570573fd38ab31c0438e9d364c6
NebSehemvi <31417380+NebSehemvi@users.noreply.github.com>
2017-09-04 16:59:54 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+var static = require('node-static');
+var mdls = require('./lib/mdls.js'); // моя библиотека модулей
+var file = new static.Server('.');
+
+http.createServer(function(req, res) {
+ if (req.method == 'GET') {
+  file.serve(req, res);
+  }
+}).listen(8080);
+
+console.log('Server running on port 8080');
